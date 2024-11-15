d236b28436586f52e9196337b1a367286a61ee76
Celtenn <enescelten@gmail.com>
2024-11-15 20:22:27 +0300

    Add files via upload

---
+++ get_next_line_utils.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+char *ft_strchr(char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (str + i);
+		i++;
+	}
+	return (str);
+}
+
+char *ft_strjoin(const char *str, const char *nbr)
+{
+	int	p;
+	int	i;
+	char	*arr;
+
+	i = 0;
+	arr = (char *)malloc(ft_strlen(str) + ft_strlen(nbr));
+	while (str[i])
+	{
+		arr[p] = str[i];
+		i++;
+		p++;
+	}
+	i = 0;
+	while (nbr[i])
+	{
+		arr[p + i] = nbr[i];
+		i++;
+	}
+	return (arr);
+}
+
+#include <stdio.h>
+int main()
+{
+	char nbr[] = "merhaba";
+	printf("%s", ft_strjoin(nbr, "lan"));
+}
